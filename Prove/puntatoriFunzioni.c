d71acbb8b0075cf871c61ec3683c4337754050fe
Giustino Borzacchiello <giustinob@gmail.com>
2011-04-06 10:53:12 +0200

    Aggiunto esempio di menu che richiama funzioni tramite puntatori memorizzati in un array

--- Prove/puntatoriFunzioni.c
+++ Prove/puntatoriFunzioni.c
@@ -5,7 +5,38 @@
 //       ^
 //       |___Accanto a questo * ci dovrebbe essere il nome della funzione: è anonimo perché mi serve solo per il cast
 
-int NumComp(int Num1, int Num2)
+int NumComp(int Num1, int Num2);
+void function1( int );
+void function2( int );
+void function3( int );
+
+void MiaFunzione( void *A, void *B, int (*Comp)(void *, void *) )
+{
+	printf("%s|%s: %d\n", A, B, (*Comp)((void *)A,(void *)B));
+}
+
+int main( int argc, char **argv )
+{
+    //int a = atoi(argv[1]);
+	//int b = atoi(argv[2]);
+    void (*f[ 3 ])( int ) = { function1, function2, function3 };
+	int choice;
+
+	printf("Immettere un numero tra 0 e 2, 3 per terminare\n");
+	scanf("%d", &choice);
+
+	while( choice >= 0 && choice < 3 )
+	{
+		(*f[choice])( choice );
+		printf("\nImmettere un numero tra 0 e 2, 3 per terminare\n");
+		scanf("%d", &choice);
+	}
+    //MiaFunzione(a, b, (int (*)(int,int))(NumComp) );
+    //MiaFunzione(argv[1], argv[2], (int (*)(void *, void *)) strcmp );
+	return 0;
+}
+
+int NumComp(int Num1, int Num2) 
 {
 	int ReturnValue; /**< Valore di ritorno della funzione */
 
@@ -25,17 +56,17 @@ int NumComp(int Num1, int Num2)
 	return ReturnValue; 
 }
 
-void MiaFunzione( void *A, void *B, int (*Comp)(void *, void *) )
+void function1( int a )
 {
-	printf("%s|%s: %d\n", A, B, (*Comp)((void *)A,(void *)B));
+	printf("Hai immesso %d percio' è stata chiamata function1", a+1);
 }
 
-int main( int argc, char **argv )
+void function2( int b )
 {
-    int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
+	printf("Hai immesso %d percio' è stata chiamata function2", b+1);
+}
 
-    //MiaFunzione(a, b, (int (*)(int,int))(NumComp) );
-    MiaFunzione(argv[1], argv[2], (int (*)(void *, void *)) strcmp );
-	return 0;
+void function3( int c )
+{
+	printf("Hai immesso %d percio' è stata chiamata function3", c+1);
 }
