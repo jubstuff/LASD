0d74aeab98245e2cecad24a2874eff4068c01fb0
Giustino Borzacchiello <giustinob@gmail.com>
2011-10-18 08:18:44 +0200

    Added mem interface for memory management

---
+++ mem/mem.c
@@ -0,0 +1,53 @@
+/** 
+ * mem.c
+ *
+ * implementation of the mem interface
+ *
+ * */
+
+#include <stdlib.h>
+#include <stddef.h>
+#include "mem.h"
+
+void *Mem_alloc(long nbytes, const char *file, int line)
+{
+	void *ptr;
+
+	ptr = malloc(nbytes);
+	if( ptr == NULL )
+	{
+		//TODO RAISE ERROR
+	}
+	return ptr;
+}
+
+void *Mem_calloc(long count, long nbytes, const char *file, int line)
+{
+	void *ptr;
+
+	ptr = calloc(count, nbytes);
+	if( ptr == NULL )
+	{
+		//TODO RAISE ERROR
+	}
+
+	return ptr;
+}
+
+void *Mem_free(void *ptr, const char *file, int line)
+{
+	if( ptr )
+	{
+		free(ptr);
+	}
+}
+
+void *Mem_resize(void *ptr, long nbytes, const char *file, int line)
+{
+	ptr = realloc(ptr, nbytes);
+	if( ptr == NULL )
+	{
+		//TODO RAISE ERROR
+	}
+	return ptr;
+}
