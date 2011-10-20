4be7323054028b7aa78586633df1256e9a77bf49
Giustino Borzacchiello <giustinob@gmail.com>
2011-10-20 18:03:14 +0200

    Moved mem module to src

---
+++ src/mem.h
@@ -0,0 +1,35 @@
+/**
+ * mem.h
+ *
+ * Abstraction for C's standard library memory management functions
+ *
+ * */
+#include "errors.h"
+
+#ifndef MEM_H
+#define MEM_H
+
+extern void *Mem_alloc( long nbytes, const char *file, int line);
+extern J_STATUS MemAlloc( long nbytes, void *OutPtr);
+extern void *Mem_calloc( long count, long nbytes, const char *file, int line);
+extern void Mem_free(void *ptr, const char *file, int line);
+extern void *Mem_resize(void *ptr, long nbytes, const char *file, int line);
+
+
+#define ALLOC(nbytes) \
+	Mem_alloc((nbytes), __FILE__, __LINE__)
+
+#define CALLOC(nbytes) \
+	Mem_calloc((nbytes), __FILE__, __LINE__)
+
+#define FREE(ptr) ((void)(Mem_free((ptr), \
+				__FILE__, __LINE__), (ptr) = 0))
+
+#define RESIZE(ptr, nbytes) ((ptr) = Mem_resize((ptr), \
+			(nbytes), __FILE__, __LINE__))
+
+#define  NEW(p) ((p) = ALLOC((long)sizeof *(p)))
+
+#define NEWO(p) ((p) = CALLOC(1, (long)sizeof *(p)))
+
+#endif
