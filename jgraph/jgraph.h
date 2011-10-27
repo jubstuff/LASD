e42b23fbe5ef087c2e261db9f7c72d724121ad07
Giustino Borzacchiello <giustinob@gmail.com>
2011-10-27 21:04:08 +0200

    Prima bozza di interfaccia del grafo

--- jgraph/jgraph.h
+++ jgraph/jgraph.h
@@ -5,16 +5,48 @@
  *
  * Autore : Giustino Borzacchiello
  * */
+#ifndef JGRAPH_H
+#define JGRAPH_H
 
-#define NUM_VERTICES 100
+typedef struct vertex_tag J_VERTEX;
 
-typedef struct jedge_tag {
-	char *Label;
-	double *Weight;
-} J_EDGE;
+typedef struct graph_op_tag JGRAPH_METHODS;
 
-typedef struct jgraph_tag {
-	J_EDGE AdjLists[NUM_VERTICES];
-} J_GRAPH;
+typedef struct graph_tag J_GRAPH;
 
-J_GRAPH InitializeGraph( );
+/*
+ * Funzioni per la gestione del grafo
+ * */
+
+/*
+ * Inizializza il grafo ed i relativi metodi
+ * */
+J_STATUS JGraph_Init( J_GRAPH **G, JGRAPH_METHODS *Op );
+
+/**
+ * Aggiunge un vertice con etichetta Label e dati associati al grafo
+ * */
+J_STATUS JGraph_AddVertex( char *Label, void *Data, J_GRAPH *G );
+
+/**
+ * Aggiunge un arco (con peso Weight) al grafo, dal vertice con etichetta SourceLabel al vertice
+ * con etichetta DestLabel
+ * */
+J_STATUS JGraph_AddEdge( char *SourceLabel, char *DestLabel, double Weight, J_GRAPH *G);
+
+/**
+ * Rimuove dal grafo il vertice con etichetta Label (se esiste)
+ * */
+J_STATUS JGraph_RemoveVertex( char *Label, J_GRAPH *G );
+
+/**
+ * Rimuove dal grafo l'arco tra i vertici identificati da SourceLabel e DestLabel
+ * */
+J_STATUS JGraph_RemoveEdge( char *SourceLabel, char *DestLabel, J_GRAPH *G );
+
+/**
+ * Dealloca tutti i vertici, tutti gli archi ed il grafo stesso
+ * */
+void     JGraph_Destroy( J_GRAPH *G );
+
+#endif
