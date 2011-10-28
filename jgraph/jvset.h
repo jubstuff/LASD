b9fc05ca98afd154fde9d90370c8c0ff15951b51
Giustino Borzacchiello <giustinob@gmail.com>
2011-10-28 16:59:16 +0200

    Prima bozza di interfaccia jvset

--- jgraph/jvset.h
+++ jgraph/jvset.h
@@ -18,6 +18,29 @@
 
 typedef struct jvset_tag J_VSET;
 
-J_STATUS
+/**
+ * Inizializza l'insieme
+ * */
+J_STATUS JVset_Init( J_VSET **Set );
+
+/**
+ * Aggiunge un vertice con etichetta Label e dati associati all'insieme
+ * */
+J_STATUS JVset_AddVertex( char *Label, void *Data, J_VSET *Set );
+
+/**
+ * Rimuove un vertice dall'insieme
+ * */
+J_STATUS JVset_RemoveVertex( char *Label, J_VSET *Set );
+
+/**
+ * Recupera l'informazione associata al vertice
+ * */
+J_STATUS JVset_GetVertexData( char *Label, void *Data, J_VSET *Set );
+
+/**
+ * Dealloca l'insieme di vertici
+ * */
+void JVset_Destroy( J_VSET *Set );
 
 #endif
