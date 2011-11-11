92fc7a6c47c7b5f55a91514d045188c8a91837ba
Giustino Borzacchiello <giustinob@gmail.com>
2011-11-11 19:42:49 +0100

    Creata interfaccia per insieme archi

---
+++ jgraph/jeset.h
@@ -0,0 +1,44 @@
+/**
+ * Definizione interfaccia per l'insieme degli archi di un grafo
+ *
+ * eset sta per "edges set"
+ *
+ *
+ * Data: 11/11/11
+ *
+ * Autore: Giustino Borzacchiello
+ *
+ *
+ * */
+#ifndef JESET_H
+#define JESET_H
+
+#include "errors.h"
+
+/**
+ * DEFINIZIONE TIPI DI DATO
+ * */
+
+typedef struct jeset_tag J_ESET;
+
+/**
+ * Inizializza l'insieme degli archi
+ * */
+J_STATUS JEset_New( J_ESET **Set );
+
+/**
+ * Aggiunge un arco all'insieme
+ * */
+J_STATUS JEset_AddEdge( char *LabelSource, char *LabelDest, double Weigth, J_ESET *Set );
+
+/**
+ * Rimuove un arco dall'insieme
+ * */
+J_STATUS JEset_RemoveEdge( char *LabelSource, char *LabelDest, J_ESET *Set );
+
+/**
+ * Dealloca l'insieme degli archi
+ * */
+void JEset_Destroy( J_ESET *Set );
+
+#endif
