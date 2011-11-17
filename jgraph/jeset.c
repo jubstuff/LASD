8029c9f1c77c4fcdc664cc7c6f3387208549b93a
Giustino Borzacchiello <giustinob@gmail.com>
2011-11-17 10:12:35 +0100

    Aggiunte le prime funzioni per la gestione delle liste di adiacenza

--- jgraph/jeset.c
+++ jgraph/jeset.c
@@ -16,3 +16,46 @@ struct jeset_tag
 
 
 /*=======================FINE DEFINIZIONE STRUTTURE==============================*/
+
+/*============================METODI PRIVATI=====================================*/
+static void *InizializzaNodoAdjList( void *Value );
+static int EdgeCmp( const void *Edge1, const void *Edge2 );
+static void DeallocaEdge( void *InputValue, void *NodeInfo );
+
+/*=========================GESTIONE LISTE DI ADIACENZA===========================*/
+
+static void *InizializzaNodoAdjList( void *Value )
+{
+	J_EDGE *Edge = NULL;
+	MemAlloc( sizeof(J_EDGE), (void **)&Edge );
+	*Edge = *( (J_EDGE *)Value );
+	return (void *)Edge;
+}
+
+static int EdgeCmp( const void *Edge1, const void *Edge2 )
+{
+	int ReturnValue;
+	J_VERTEX *First = (J_VERTEX *)Edge1;
+	J_VERTEX *Second = (J_VERTEX *)Edge2;
+
+	if ( First->Id < Second->Id )
+	{
+		ReturnValue = -1;
+	}
+	else if ( First->Id == Second->Id )
+	{
+		ReturnValue = 0;
+	}
+	else
+	{
+		ReturnValue = 1;
+	}
+
+	return ReturnValue;
+}
+
+static void DeallocaEdge( void *InputValue, void *NodeInfo )
+{
+	free( NodeInfo );
+}
+
