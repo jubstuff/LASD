94f5fb6e93c8d03e235120fef09d91ebdefb2867
Giustino Borzacchiello <giustinob@gmail.com>
2011-11-17 10:20:41 +0100

    Creati test per Edge e aggiunti include

---
+++ test/TestEdge.c
@@ -0,0 +1,19 @@
+#include "mem.h"
+#include "unity.h"
+#include "errors.h"
+#include <string.h>
+#include "lista.h"
+#include <stdlib.h>
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+
+void test_Test(void)
+{
+}
